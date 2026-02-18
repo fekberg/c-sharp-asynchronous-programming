8a7d7774ccc8bf5320bf85e08b7909abae4ab6cb
Filip Ekberg <filip@ekberg.dev>
2026-02-18 20:30:45 +0100

    Add missing file

---
+++ src/Cross-Platform/04/Start_Here/StockAnalyzer.CrossPlatform/Services/StockStreamService.cs
@@ -0,0 +1,11 @@
+﻿using StockAnalyzer.Core.Domain;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace StockAnalyzer.Windows.Services;
+
+public interface IStockStreamService
+{
+    IAsyncEnumerable<StockPrice>
+        GetAllStockPrices(CancellationToken cancellationToken = default);
+}
