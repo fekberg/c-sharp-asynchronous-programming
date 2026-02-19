c85e718ea522f3dc15ae71af917af715934c9c79
Filip Ekberg <filip@ekberg.dev>
2026-02-19 10:47:03 +0100

    Add missing file

---
+++ src/Windows/04/Start_Here/StockAnalyzer.Windows/StateMachineDemo.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace StockAnalyzer.Windows;
+
+public class StateMachineDemo
+{
+    public async Task<string> Run() 
+    {
+        return await Compute();
+    }
+
+    public async Task<string> Compute()
+    {
+        return await Load();
+    }
+
+    public async Task<string> Load()
+    {
+        return await Task.Run(() => "Pluralsight");
+    }
+}
