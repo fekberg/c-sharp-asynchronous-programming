a5a1d98a8d48bb68c00c472b3020582bde94d89e
Filip Ekberg <filip@ekberg.dev>
2026-02-20 11:16:50 +0100

    Update to newer Lock

--- src/Cross-Platform/07/Working with Shared Variables (Completed)/StockAnalyzer.AdvancedTopics/Program.cs
+++ src/Cross-Platform/07/Working with Shared Variables (Completed)/StockAnalyzer.AdvancedTopics/Program.cs
@@ -1,39 +1,31 @@
-﻿using System.Diagnostics;
+﻿
+using System.Diagnostics;
 
-namespace StockAnalyzer.AdvancedTopics;
+Stopwatch stopwatch = new();
+stopwatch.Start();
 
-internal class Program
+Lock lockObject = new();
+decimal total = 0;
+
+Parallel.For(0, 100, (i) => {
+    var result = Compute(i);
+    lock (lockObject) // Only lock for a short time!
+    {
+        total += result;
+    }
+});
+
+Console.WriteLine(total);
+Console.WriteLine($"It took: {stopwatch.ElapsedMilliseconds}ms to run");
+Console.ReadLine();
+
+decimal Compute(int value)
 {
-    static object syncRoot = new();
-    static void Main(string[] args)
-    {
-        Stopwatch stopwatch = new();
-        stopwatch.Start();
+    var randomMilliseconds = Random.Shared.Next(10, 50);
+    var end = DateTime.Now + TimeSpan.FromMilliseconds(randomMilliseconds);
 
-        decimal total = 0;
+    // This will spin for a while...
+    while (DateTime.Now < end) { }
 
-        Parallel.For(0, 100, (i) => {
-            var result = Compute(i);
-            lock (syncRoot) // Only lock for a short time!!
-            {
-                total += result;
-            }
-        });
-
-        Console.WriteLine(total);
-        Console.WriteLine($"It took: {stopwatch.ElapsedMilliseconds}ms to run");
-        Console.ReadLine();
-    }
-
-    static Random random = new();
-    static decimal Compute(int value)
-    {
-        var randomMilliseconds = random.Next(10, 50);
-        var end = DateTime.Now + TimeSpan.FromMilliseconds(randomMilliseconds);
-
-        // This will spin for a while...
-        while(DateTime.Now < end) { }
-
-        return value + 0.5m;
-    }
+    return value + 0.5m;
 }
